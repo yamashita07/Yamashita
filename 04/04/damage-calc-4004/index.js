e17cb9cbd6e7145d3779f7764ae88419529b914e
yamashita <t_yamashita@arssystem.co.jp>
2019-04-22 16:07:29 +0900

    mochaとテスティングフレームワーク

--- 04/04/damage-calc-4004/index.js
+++ 04/04/damage-calc-4004/index.js
@@ -16,8 +16,9 @@
  */
 function effectiveDamage(power, armor, armorPenetration) {
   let effectiveArmor = normalize(armor) - normalize(armorPenetration);
+  effectiveArmor = effectiveArmor <= 0 ? 0 : effectiveArmor;
   const damageDecrease = effectiveArmor / (100 + effectiveArmor);
-  return Math.floor(normalize(power) * (1 - damageDecrease));
+  return Math.round(normalize(power) * (1 - damageDecrease));
 }
 
 /**
@@ -28,6 +29,8 @@ function effectiveDamage(power, armor, armorPenetration) {
 function normalize(n) {
   if (n < 0) {
     return 0;
+  } else if (n >= 2000) {
+    return 2000;
   } else {
     return n;
   }
