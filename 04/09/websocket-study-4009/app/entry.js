42173731bd81518e47cefdbae1fc8e34388b1bed
yamashita <t_yamashita@arssystem.co.jp>
2019-04-23 16:06:46 +0900

    WebSocketでプッシュ通信の方式を実現

---
+++ 04/09/websocket-study-4009/app/entry.js
@@ -0,0 +1,31 @@
+'use strict';
+import $ from 'jquery';
+const block = $('#block');
+const scalingButton = $('#scaling-button');
+
+scalingButton.click(() => {
+  block.animate({ width: '200pt', height: '200pt' }, 2000);
+  block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
+
+const movingButton = $('#moving-button');
+
+movingButton.click(() => {
+  block.animate({ 'marginLeft': '500px' }, 500);
+  block.animate({ 'marginLeft': '20px' }, 1000);
+});
+
+const loadavg = $('#loadavg');
+
+import io from 'socket.io-client';
+const socket = io('http://localhost:8000');
+socket.on('server-status', (data) => {
+  loadavg.text(data.loadavg.toString());
+});
+
+socket.on('connect', () => {
+  console.log('接続しました');
+});
+socket.on('disconnect', () => { 
+  console.log('切断しました'); 
+});
